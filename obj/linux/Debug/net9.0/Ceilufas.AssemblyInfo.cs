31d30820ce6badfe61ac731b0d8fb688356e7356
Djellal Elkacem <djellal.elkacem@gmail.com>
2025-04-06 19:47:32 +0000

    feat(login): allow login with username or email
    Modify the login component to accept either a username or email for authentication. Update the input field and validation logic to handle both cases. This improves user flexibility and experience.

--- obj/linux/Debug/net9.0/Ceilufas.AssemblyInfo.cs
+++ obj/linux/Debug/net9.0/Ceilufas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ceilufas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f29a51d83d31e5fb0f42a8a7114a59bec1f78bd2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b011e744fd4c91cbd318cb25c6f0356291120fb4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ceilufas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ceilufas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
